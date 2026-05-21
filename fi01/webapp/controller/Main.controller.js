bc59a2c6668791fef5d680dfcad8afb7a155b547
dayoungchu <cnekdud928@kookmin.ac.kr>
2026-05-21 18:41:11 +0900

    2026.05.21

---
+++ fi01/webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
+    "use strict";
+
+    return Controller.extend("code.t4.ui5.fi01.controller.Main", {
+        onInit() {
+        }
+    });
+});
